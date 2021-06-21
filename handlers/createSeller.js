e782e122912bcf1e525bea631c62f79251d05bd8
Sharran <sharran.murali@gmail.com>
2021-06-21 23:37:29 +0530

    TODO updated

--- handlers/createSeller.js
+++ handlers/createSeller.js
@@ -3,6 +3,7 @@ const ULID = require('ulid')
 let response;
 exports.lambdaHandler = async (event) => {
     try {
+        //TODO: Construct valid body and save in DynamoDB
         let seller = JSON.parse(event.body)
         seller.PK = "SELLER#" + ULID.ulid()
         seller.SK = "#SELLER#" + ULID.ulid()
